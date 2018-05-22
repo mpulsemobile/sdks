c5693ffdb6dec8ab8faa1d4c405476b63c1b9a74
Heena Dhawan <dhawanheena26@gmail.com>
2018-05-22 23:09:48 +0530

    Adding framework in folder for future use

---
+++ sample/mPulseSampleApp/mPulseSampleApp/MpulseFramework.framework/Headers/MpulseFramework.h
@@ -0,0 +1,20 @@
+//
+//  MpulseFramework.h
+//  MpulseFramework
+//
+//  Created by mPulse Team on 10/04/18.
+//  Copyright © 2018 mPulse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MpulseFramework/MpulseHandler.h>
+#import <MpulseFramework/MpulseInboxView.h>
+
+//! Project version number for MpulseFramework.
+FOUNDATION_EXPORT double MpulseFrameworkVersionNumber;
+
+//! Project version string for MpulseFramework.
+FOUNDATION_EXPORT const unsigned char MpulseFrameworkVersionString[];
+
+
+
